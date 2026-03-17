e609d04d0bdc31ef7732411f7567752cd5d18ade
rthaylan00-sketch <rthaylan00@gmail.com>
2026-03-16 23:07:54 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 // ===== CONFIGURAÇÃO DO FIREBASE =====
 const firebaseConfig = {
-  apiKey: "AIzaSyCeQD2PNlXf2j8lTbit6kt",
+  apiKey: "AIzaSyCeQD2PNlXf2j8lTbit6ktOZR2FtAufLbY",
   authDomain: "agendapro-788d0.firebaseapp.com",
   projectId: "agendapro-788d0",
   storageBucket: "agendapro-788d0.firebasestorage.app",
   messagingSenderId: "627296929583",
-  appId: "1:627296929583:web:d1773f217"
+  appId: "1:627296929583:web:d1773f21704407b41a43da",
+  measurementId: "G-66SHNPQPMR"
 };
+
 
 firebase.initializeApp(firebaseConfig);
 const db = firebase.firestore();
