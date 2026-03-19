48826a718c8a17d62d81105b5bb80d8718be9b82
rthaylan00-sketch <rthaylan00@gmail.com>
2026-03-18 23:23:57 -0300

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,3 +1,8 @@
+// Importações
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
+
+// Sua config (já está certa)
 const firebaseConfig = {
   apiKey: "AIzaSyCeQD2PNlXf2j8lTbit6ktOZR2FtAufLbY",
   authDomain: "agendapro-788d0.firebaseapp.com",
@@ -6,3 +11,12 @@ const firebaseConfig = {
   messagingSenderId: "627296929583",
   appId: "1:627296929583:web:d1773f21704407b41a43da"
 };
+
+// Inicializa Firebase
+const app = initializeApp(firebaseConfig);
+
+// Inicializa Firestore
+const db = getFirestore(app);
+
+// Exporta o banco
+export { db };
