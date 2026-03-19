14d162b01dd6a443a4e6bfb8e67c73b1fb81e5ce
rthaylan00-sketch <rthaylan00@gmail.com>
2026-03-18 23:22:54 -0300

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,1 +1,8 @@
-
+const firebaseConfig = {
+  apiKey: "AIzaSyCeQD2PNlXf2j8lTbit6ktOZR2FtAufLbY",
+  authDomain: "agendapro-788d0.firebaseapp.com",
+  projectId: "agendapro-788d0",
+  storageBucket: "agendapro-788d0.appspot.com",
+  messagingSenderId: "627296929583",
+  appId: "1:627296929583:web:d1773f21704407b41a43da"
+};
