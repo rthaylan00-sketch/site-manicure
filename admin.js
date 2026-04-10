a33ff2dda0583258dcf0c9d09ad284b095e484f7
rthaylan00-sketch <rthaylan00@gmail.com>
2026-04-10 17:12:17 -0300

    Add Firebase configuration to admin.js

--- admin.js
+++ admin.js
@@ -1,3 +1,21 @@
+// ===== FIREBASE =====
+const firebaseConfig = {
+  apiKey: "AIzaSyCeQD2PNlXf2j8lTbit6ktOZR2FtAufLbY",
+  authDomain: "agendapro-788d0.firebaseapp.com",
+  projectId: "agendapro-788d0",
+  storageBucket: "agendapro-788d0.appspot.com",
+  messagingSenderId: "627296929583",
+  appId: "1:627296929583:web:d1773f21704407b41a43da"
+};
+
+if (!firebase.apps.length) {
+  firebase.initializeApp(firebaseConfig);
+}
+
+const db = firebase.firestore();
+
+
+
 function verificarSenha() {
   const senha = document.getElementById('campo-senha').value;
   if (senha === "Lu1234") {
